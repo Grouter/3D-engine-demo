f89e61c2098480b0021b63ecbebe8dd9cb8c7b94
Grouter <matus.perfekt@gmail.com>
2021-10-08 15:51:22 +0200

    normals, depth testing and culling

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -1,12 +1,17 @@
 #ifdef VERTEX
 
 in vec3 position;
+in vec3 normal;
 
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model; // @Temporary
 
+out vec3 c;
+
 void main() {
+    c = normal;
+
     gl_Position = projection * view * model * vec4(position, 1.0);
 }
 
@@ -14,10 +19,12 @@ void main() {
 
 #ifdef FRAGMENT
 
+in vec3 c;
+
 out vec4 fragment_color;
 
 void main() {
-    fragment_color = vec4(0.0, 0.0, 0.0, 1.0);
+    fragment_color = vec4(c, 1.0);
 }
 
 #endif
