794cfa751fcbc3d6ed95ca1a2bb61437177b69cb
Grouter <matus.perfekt@gmail.com>
2021-10-14 17:44:35 +0200

    moved load_model function

--- src/resources.cpp
+++ src/resources.cpp
@@ -60,3 +60,117 @@ u32 create_white_texture() {
 
     return texture;
 }
+
+// @Todo: This mesh loading is wasting a lot of memory!
+// We allocate the same amount of verticies and normals as we have indicies.
+// I think some of the data is always duplicate (which wastes memory)...
+// We should actually backcheck if the vertex already exists and if not, only then add it.
+internal Mesh load_model(const char *name) {
+    printf("Loading %s 3D model\n", name);
+
+    tinyobj::ObjReader reader;
+
+    bool result = reader.ParseFromFile(name);
+    if (!result) {
+        printf("Error loading %s 3D model file\n", name);
+        if (!reader.Error().empty()) {
+            printf(reader.Error().c_str());
+        }
+    }
+
+    if (!reader.Warning().empty()) {
+        printf(reader.Warning().c_str());
+    }
+
+    tinyobj::attrib_t              data   = reader.GetAttrib();
+    std::vector<tinyobj::shape_t>  shapes = reader.GetShapes();
+
+    tinyobj::mesh_t *sub_mesh;
+
+    u64 index_count = 0;
+
+    // This just counts all the indicies of all sub meshes so we can
+    // allocate space for them. Using a dynamic array would be very slow...
+    for (u64 shape_index = 0; shape_index < shapes.size(); shape_index++) {
+        sub_mesh = &shapes[shape_index].mesh;
+
+        // We export .obj with triangulate faces option, so every face has exactly 3 verticies.
+        u64 sub_mesh_indicies = sub_mesh->num_face_vertices.size() * 3;
+        index_count += sub_mesh_indicies;
+    }
+
+    Mesh mesh = {};
+
+    allocate_array(mesh.sub_meshes, shapes.size());
+    allocate_array(mesh.verticies,  index_count);
+    allocate_array(mesh.normals,    index_count);
+    allocate_array(mesh.indicies,   index_count);
+    allocate_array(mesh.uvs,        index_count);
+
+    for (u64 shape_index = 0; shape_index < shapes.size(); shape_index++) {
+        u64 index_offset = 0;
+        sub_mesh = &shapes[shape_index].mesh;
+
+        SubMeshInfo sub_mesh_info = {};
+
+        sub_mesh_info.first_index = (u32)mesh.indicies.length;
+        sub_mesh_info.index_count = (u32)sub_mesh->indices.size();
+
+        mesh.sub_meshes.add(sub_mesh_info);
+
+        for (u64 f = 0; f < sub_mesh->num_face_vertices.size(); f++) {
+            u64 fv = (u64)sub_mesh->num_face_vertices[f];
+
+            for (u64 v = 0; v < fv; v++) {
+                tinyobj::index_t idx = sub_mesh->indices[index_offset + v];
+
+                // Indicies
+                mesh.indicies.add((u32)(index_offset + v));
+
+                // Position
+                {
+                    Vector3 position = {};
+
+                    position.x = data.vertices[3 * (u64)idx.vertex_index + 0];
+                    position.y = data.vertices[3 * (u64)idx.vertex_index + 1];
+                    position.z = data.vertices[3 * (u64)idx.vertex_index + 2];
+
+                    mesh.verticies.add(position);
+                }
+
+                // Normals
+                if (idx.normal_index >= 0) {
+                    Vector3 normal = {};
+
+                    normal.x = data.normals[3 * (u64)idx.normal_index + 0];
+                    normal.y = data.normals[3 * (u64)idx.normal_index + 1];
+                    normal.z = data.normals[3 * (u64)idx.normal_index + 2];
+
+                    mesh.normals.add(normal);
+                }
+
+                // UVs
+                if (idx.texcoord_index >= 0) {
+                    Vector2 uv = {};
+
+                    uv.x = data.texcoords[2 * (u64)idx.texcoord_index + 0];
+                    uv.y = data.texcoords[2 * (u64)idx.texcoord_index + 1];
+
+                    mesh.uvs.add(uv);
+                }
+            }
+
+            index_offset += fv;
+        }
+    }
+
+    printf("Mesh sub meshes: %llu\n", mesh.sub_meshes.length);
+    printf("Mesh verticies:  %llu\n", mesh.verticies.length);
+    printf("Mesh normals:    %llu\n", mesh.normals.length);
+    printf("Mesh indicies:   %llu\n", mesh.indicies.length);
+    printf("Mesh uv:         %llu\n", mesh.uvs.length);
+
+    bind_mesh_buffer_objects(mesh);
+
+    return mesh;
+}
