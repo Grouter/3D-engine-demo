ac47f94275b79727da29f06926d44fae8978a85c
hrumy <jakubko.kopka@gmail.com>
2021-12-15 15:13:59 +0100

    added objects/flame

--- src/particle.h
+++ src/particle.h
@@ -3,9 +3,11 @@
 
 #define PARTICLE_AMMOUNT 2048
 #define ROOT_POSITION {-9.5f, 0.25f, -16.77f}
+#define FLAME_ROOT_POSITION {9.0f, 4.965f, -34.99f}
 #define SPAWN_OFFSET 0.35f
-#define FIRE_SPAWNRATE 0.05f
-#define SMOKE_SPAWNRATE 0.02f
+#define FIRE_SPAWNRATE 0.04f
+#define SMOKE_SPAWNRATE 0.03f
+#define FLAME_SPAWNRATE 2.0f
 
 struct Particle {
     Vector3 position;
@@ -18,9 +20,11 @@ struct Particle {
     u8 type;
 };
 
-global Array<Particle> particles;
-global f32 fire_spawnrate_timer  = 0;
-global f32 smoke_spawnrate_timer = 0;
+Array<Particle> particles;
+f32 fire_spawnrate_timer  = 0;
+f32 smoke_spawnrate_timer = 0;
+f32 flame_spawnrate_timer = 0;
+const Vector3 WIND = make_vector3(-0.4f, 0.0f, 0.2f);
 
 
 #endif
